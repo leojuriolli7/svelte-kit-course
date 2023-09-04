ba5559e9a1bc6e366336fec6d629f6404cfc7875
leojuriolli7 <leojuriolli@gmail.com>
2023-09-03 22:28:59 -0300

    feat: expected error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>Root Error Page</h1>
+
+<h2>{$page.status} - {$page.error?.message}</h2>
---
+++ src/routes/products/[id=int]/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>Product ID Error Page</h1>
+
+<h2>{$page.status} - {$page.error?.message}</h2>
